1d718eb18ab9cdf1d7d78bbbd3efdfead85aaeb6
jingzhnjun <925102515@qq.com>
2019-06-12 10:45:49 +0800

    sign this is master

--- src/main/java/com/blue/zuulgateway/ZuulGatewayApplication.java
+++ src/main/java/com/blue/zuulgateway/ZuulGatewayApplication.java
@@ -10,6 +10,7 @@ public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulGatewayApplication.class, args);
+        System.out.println("this is master");
     }
 
 }
