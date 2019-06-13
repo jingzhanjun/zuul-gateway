b7a2bb06ab200bb56c3915d6530d4cda43f0b5ae
jingzhnjun <925102515@qq.com>
2019-06-13 10:40:03 +0800

    add test.txt

--- src/main/java/com/blue/zuulgateway/ZuulGatewayApplication.java
+++ src/main/java/com/blue/zuulgateway/ZuulGatewayApplication.java
@@ -11,7 +11,7 @@ public class ZuulGatewayApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ZuulGatewayApplication.class, args);
-        System.out.println("this is dev2");
+        System.out.println("this is dev5");
     }
 
 }
